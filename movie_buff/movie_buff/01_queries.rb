bd28680900d4542368bd19e43d6867846901b8aa
ortizjs <jonnatano94@gmail.com>
2018-11-08 14:09:26 -0800

    Finish biggest_cast spec

--- movie_buff/movie_buff/01_queries.rb
+++ movie_buff/movie_buff/01_queries.rb
@@ -42,7 +42,13 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
-
+  Movie
+  .select(:id, :title)
+  .joins(:castings)
+  .group(:id)
+  .order('COUNT(*) DESC')
+  .limit(3)
+  
 end
 
 def directed_by_one_of(them)
