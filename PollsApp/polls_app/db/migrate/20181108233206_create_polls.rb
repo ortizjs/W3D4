38d913817e2f0d8bc4efe81074a1a517fd655d88
ortizjs <jonnatano94@gmail.com>
2018-11-08 15:34:53 -0800

    Create poll migration

--- PollsApp/polls_app/db/migrate/20181108233206_create_polls.rb
+++ PollsApp/polls_app/db/migrate/20181108233206_create_polls.rb
@@ -1,8 +1,10 @@
 class CreatePolls < ActiveRecord::Migration[5.1]
   def change
     create_table :polls do |t|
-
+      t.string :title, null: false, unique: true
+      t.integer :user_id, null: false
       t.timestamps
     end
+    add_index :polls, :user_id, unique: true
   end
 end
