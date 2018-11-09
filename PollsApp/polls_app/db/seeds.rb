242d04b5e8138c7741bb0ee2f31901c703622c12
ortizjs <jonnatano94@gmail.com>
2018-11-08 16:51:29 -0800

    Create seeds for questions and polls

--- PollsApp/polls_app/db/seeds.rb
+++ PollsApp/polls_app/db/seeds.rb
@@ -6,9 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+
 jon = User.create(username: "jondoom")
 jon2 = User.create(username: "jonortiz")
 daniel = User.create(username: "danielmoon")
 ted = User.create(username: "tedwild")
 sameeh = User.create(username: "sameeh")
 
+Poll.destroy_all
+
+aa = Poll.create(title: "App Academy", user_id: jon.id)
+ab = Poll.create(title: "Forgets", user_id: jon2.id)
+ac = Poll.create(title: "Background", user_id: ted.id)
+
+Question.destroy_all
+
+q1 = Question.create(body: "How many hours do you study?", poll_id: aa.id)
+q2 = Question.create(body: "Who is your favorite TA?", poll_id: aa.id)
+q3 = Question.create(body: "What pod do you belong to?", poll_id: aa.id)
+
+q4 = Question.create(body: "How many quizzes have you forgotten?", poll_id: ab.id)
+q5 = Question.create(body: "How many strikes do you have?", poll_id: ab.id)
+q6 = Question.create(body: "How many reports did you forget?", poll_id: ab.id)
+
+q7 = Question.create(body: "Where are you from?", poll_id: ac.id)
+q8 = Question.create(body: "What school did you go to?", poll_id: ac.id)
+q9 = Question.create(body: "What's your coding background/experience?", poll_id: ac.id)
