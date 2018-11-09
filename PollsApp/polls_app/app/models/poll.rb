a6ba60f7cc999bce8974757ddc3ab14f29223e64
ortizjs <jonnatano94@gmail.com>
2018-11-08 17:08:30 -0800

    Create validations for poll

--- PollsApp/polls_app/app/models/poll.rb
+++ PollsApp/polls_app/app/models/poll.rb
@@ -10,13 +10,15 @@
 #
 
 class Poll < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  
   belongs_to :author,
     primary_key: :id,
     foreign_key: :user_id,
     class_name: :User
     
-    has_many :questions,
-      primary_key: :id,
-      foreign_key: :poll_id,
-      class_name: :Question
+  has_many :questions,
+    primary_key: :id,
+    foreign_key: :poll_id,
+    class_name: :Question
 end
